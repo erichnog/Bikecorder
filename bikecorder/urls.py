dd7c2de5ae6000e2435e6cb973cd22eebd221b45
Erich Nogueira <erichnog@gmail.com>
2011-04-24 16:30:49 -0300

    Adding index page and some other things.

--- bikecorder/urls.py
+++ bikecorder/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 
-urlpatterns = patterns('',    
+urlpatterns = patterns('',  
     (r'^add/', 'bikecorder.views.add'),
-    (r'^movies/', 'bikecorder.views.movies'),
+    (r'', 'bikecorder.views.index'),
 )
--- bikecorder/views.py
+++ bikecorder/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render_to_response
 from django.conf import settings
 
+def index(request):
+    """docstring for fname"""
+    return render_to_response('index.html', {})
+
 def add(request):
     """docstring for add"""
-    return render_to_response('add.html', {'MURL': settings.MEDIA_ROOT})
-
-def movies(request):
-    """docstring for movies"""
-    return render_to_response('movies.html', {})
+    return render_to_response('add.html', {})
